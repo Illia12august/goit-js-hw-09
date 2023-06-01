caaac392e6fd7a97627d14c3f631fb7373976490
Illia12august <illiakrylov12@gmail.com>
2023-06-01 20:21:22 +0300

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+// Change code below this line
+const galleryContainer = document.querySelector(`.gallery`);
+
+function createGalleryContainerMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+            <li class="gallery__item">
+            <a class="gallery__link" href= "${original}">
+            <img class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+            </a>
+            </li>
+        `;
+    })
+    .join('');
+}
+const itemMarkup = createGalleryContainerMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemMarkup);
 console.log(galleryItems);
+
+// Library SimpleLightbox
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
